f8f325f82887d214e9b4ae1642cd877a9bf5e9cc
Mauricio <mauba22@outlook.com>
2024-04-24 17:24:25 -0600

    Updates

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,18 @@
-const imageSrc = document.getElementById('image'), colorPicker = document.getElementById("color-picker"), 
-  errorText = document.getElementById("alertText"), uploadedFile = document.getElementById("file"), uploadLabel = document.querySelector('.upload-image'),
-  selectedColorContainer = document.querySelector('.color-res'), colorContainer = document.querySelector('.color-selected-container'), 
-  colorPreview = document.getElementById("color-preview"),
-  hexadecimalValue = document.getElementById('hexCode'), rgbValue = document.getElementById('rgbCode'), rgbaValue = document.getElementById('rgbaCode')
-
-let eyeDropper;
+const uploadImageForm = document.querySelector('.upload-image-form'), uploadedImage = document.getElementById('uploaded-image'),
+  imageSrc = document.getElementById('image'), colorPickerButton = document.getElementById("color-picker"), errorText = document.getElementById("alertText"), 
+  colorContainer = document.querySelector('.color-selected-container'), colorPreview = document.getElementById("color-preview"),
+  hexValue = document.getElementById('hexCode'), rgbValue = document.getElementById('rgbCode'), rgbaValue = document.getElementById('rgbaCode')
+let eyeDropper
 
 window.onload = () => {
   colorPreview.style.background = 'white'
-  /* Verifica si la API es compatible con el navegador */
+  /* Verifica si la API es compatible con el navegador o no */
   if (!window.EyeDropper) {
       errorText.classList.remove('hidden')
-      selectedColorContainer.classList.add('hidden')
-      uploadedFile.disabled = true, uploadLabel.style.pointerEvents = 'none', uploadLabel.style.background = '#424649', colorPicker.disabled = true
-      errorText.innerText = "Lo sentimos, 'EyeDropper API' no es compatible con este navegador :(";
-    return;
+      colorContainer.classList.add('hidden')
+      colorPickerButton.disabled = true, uploadedImage.disabled = true
+      errorText.innerText = `\u{f071} EyeDropper API no es compatible con este navegador \u{f071}`
+    return
   } else { eyeDropper = new EyeDropper() }
 };
 
@@ -24,39 +22,40 @@ const hexColorToRgb = (color, alpha) => {
   const r = parseInt(color.substr(1, 2), 16)
   const g = parseInt(color.substr(3, 2), 16)
   const b = parseInt(color.substr(5, 2), 16)
-  rgbCodeValue =  `rgb(${r}, ${g}, ${b})`
-  rgbaCodeValue = `rgba(${r}, ${g}, ${b}, ${alpha})`
+  rgbCodeValue =  `rgb(${r}, ${g}, ${b});`, rgbaCodeValue = `rgba(${r}, ${g}, ${b}, ${alpha});`
 }
 
 /* Se inicializa la promesa que retornará el código del color */
 const initColorPicker = async () => {
   await eyeDropper.open()
     .then((colorValue) => {      
-      let hexCode = colorValue.sRGBHex;  
+      let hexCode = colorValue.sRGBHex
       hexColorToRgb(hexCode, 1)
-      hexadecimalValue.value = hexCode, rgbValue.value = rgbCodeValue, rgbaValue.value = rgbaCodeValue
+      hexValue.value = hexCode, rgbValue.value = rgbCodeValue, rgbaValue.value = rgbaCodeValue
       colorPreview.style.backgroundColor = hexCode
     })
     .catch((error) => { console.error(error) });
 };
-colorPicker.addEventListener("click", initColorPicker);
+colorPickerButton.addEventListener("click", initColorPicker);
 
-/* Se llama al evento 'change' si la imagen es modificada */
-uploadedFile.addEventListener('change', () => { 
+/* Se llama al evento 'change' si la imagen es modificada y al evento 'click' del formulario  */
+uploadImageForm.addEventListener('click', () => { uploadedImage.click() })
+uploadedImage.addEventListener('change', () => { 
   let fileReader = new FileReader() //* Objeto que permite leer los archivos del ordenador
   /* Se leé el contenido del archivo y al finalizar se asigna al atributo 'src' de la imagen */
-  fileReader.readAsDataURL(uploadedFile.files[0]);
+  fileReader.readAsDataURL(uploadedImage.files[0])
   fileReader.onload = () => {    
-    imageSrc.setAttribute("src", fileReader.result);
+    imageSrc.setAttribute("src", fileReader.result)
   };
 })
 
-const copyButtons = document.querySelectorAll('.color-res button')
+/* Copia el valor del input dependiendo el botón que se seleccionó */
+const copyButtons = document.querySelectorAll('.color-selected-container button')
 copyButtons.forEach(element => {
   element.addEventListener('click', () => {
-    var selectedInput = element.id === 'hex' ? hexadecimalValue : element.id === 'rgb' ? rgbValue : rgbaValue
+    var selectedInput = element.id === 'hex' ? hexValue : element.id === 'rgb' ? rgbValue : rgbaValue
     selectedInput.select()
     selectedInput.setSelectionRange(0, 99999)
-    navigator.clipboard.writeText(selectedInput == hexadecimalValue ? selectedInput.value.toUpperCase() : selectedInput.value)
+    navigator.clipboard.writeText(selectedInput == hexValue ? selectedInput.value.toUpperCase() : selectedInput.value)
   })
 })
